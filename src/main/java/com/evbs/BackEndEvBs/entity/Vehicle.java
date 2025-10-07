91ab23929b2a44e7d4c1989c8597136ef6fc109b
MTV-2602 <team89a6@gmail.com>
2025-10-07 13:44:31 +0700

    regex vehicle

--- src/main/java/com/evbs/BackEndEvBs/entity/Vehicle.java
+++ src/main/java/com/evbs/BackEndEvBs/entity/Vehicle.java
@@ -3,6 +3,8 @@ package com.evbs.BackEndEvBs.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,10 +23,15 @@ public class Vehicle {
     private Long id;
 
     @NotEmpty(message = "VIN cannot be empty!")
+    @Size(min = 17, max = 17, message = "VIN must be exactly 17 characters!")
     @Column(name = "VIN", nullable = false, unique = true, length = 100)
     private String vin;
 
     @NotEmpty(message = "PlateNumber cannot be empty!")
+    @Pattern(
+            regexp = "^[0-9]{2}[a-zA-Z]{1,2}[0-9]{5,6}(\\\\.[a-zA-Z]{1,2})?$",
+            message = "Invalid Vietnamese motorcycle plate format! Valid examples: 29X112345, 51F11234, 30H112350\n"
+    )
     @Column(name = "PlateNumber", nullable = false, unique = true, length = 50)
     private String plateNumber;
 

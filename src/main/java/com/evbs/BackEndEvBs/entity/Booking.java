d7fd5a24a888e87fa059a6a866e2aa3b7229b567
LePhuocTri2205 <lephuoctri2205@gmail.com>
2025-10-11 17:25:40 +0700

    fix bug Booking 2

--- src/main/java/com/evbs/BackEndEvBs/entity/Booking.java
+++ src/main/java/com/evbs/BackEndEvBs/entity/Booking.java
@@ -20,19 +20,43 @@ public class Booking {
 
     @ManyToOne
     @JoinColumn(name = "DriverID", nullable = false)
+    @JsonIgnore
     private User driver;
+
+    @Transient
+    private Long driverId;
 
     @ManyToOne
     @JoinColumn(name = "VehicleID", nullable = false)
+    @JsonIgnore
     private Vehicle vehicle;
+
+    @Transient
+    private Long vehicleId;
 
     @ManyToOne
     @JoinColumn(name = "StationID", nullable = false)
+    @JsonIgnore
     private Station station;
+
+    @Transient
+    private Long stationId;
 
     @Column(name = "BookingTime", nullable = false)
     private LocalDateTime bookingTime;
 
     @Column(name = "Status", length = 50)
     private String status = "Pending";
+
+    public Long getDriverId() {
+        return this.driver != null ? this.driver.getId() : null;
+    }
+
+    public Long getVehicleId() {
+        return this.vehicle != null ? this.vehicle.getId() : null;
+    }
+
+    public Long getStationId() {
+        return this.station != null ? this.station.getId() : null;
+    }
 }
